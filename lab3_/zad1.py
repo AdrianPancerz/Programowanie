1d14108d66f4f900bc4d50232141902a63bcc0c5
Adrian <150368955+AdrianPancerz@users.noreply.github.com>
2024-11-13 21:21:47 +0100

    Delete lab3_ directory

--- lab3_/zad1.py
+++
@@ -1,9 +0,0 @@
-Lista=["Adrian", "Michał", "Kuba", "Natalia"]
-print(sorted(Lista))
-
-Lista.append("Tomek")
-Lista.append("Sylwia")
-Lista.pop()
-Lista.insert(3,"Krzysztof")
-Lista.reverse()
-Lista1 = Lista.copy()
--- lab3_/zad2.py
+++
@@ -1,6 +0,0 @@
-tekst = "Rzeszów jest piękny"
-print(tekst[0])
-print(tekst[6])
-print(tekst[9])
-print(tekst[12])
-print(tekst[1])
